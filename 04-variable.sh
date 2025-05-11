cb8edab5075938c01aaf8a873fe2997680c931ef
raghujalnila <raghujalnila07@gmail.com>
2025-05-11 19:44:38 +0530

    hello script

--- 04-variable.sh
+++ 04-variable.sh
@@ -1,1 +1,11 @@
 
+#!/bin/bash
+
+#declaring of variable
+PERSON1=$1 #no space between = and value
+Person2=$2
+#referring variable
+echo "$1:: Hello $2, How are you?"
+echo "$2:: Hi $1, I am fine. How are you?"
+echo "$1:: I am fine too. how is your work?"
+echo "$2:: not bad. I am thinking to upgrade to DevOps"
