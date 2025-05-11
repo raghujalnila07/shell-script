0f6bd2e1f37a24243e20e264bfe9cde98a6d7fb7
raghujalnila <raghujalnila07@gmail.com>
2025-05-11 21:19:14 +0530

    hello script

---
+++ 06-array.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+MOVIES=("RRR"DjTillu"Murari")
+
+#size of above array is 3
+# index are 0,1,2
+
+#list always start with 0.
+
+echo "first Movie is:${MOVIES[0]}
+echo "first Movie is:${MOVIES[1]}
+echo "first Movie is:${MOVIES[@]}
