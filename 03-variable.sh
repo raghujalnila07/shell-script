4d4ea940009b06a3797c5ede24a5dc00de5f211a
raghujalnila <raghujalnila07@gmail.com>
2025-05-11 19:41:28 +0530

    hello script

--- 03-variable.sh
+++ 03-variable.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-P1=Raghu
-P2=Raghavendra
+#declaring of variable
+PERSON1=Raghu #no space between = and value
+Person2=Chinna
 
-echo " hi $P1"
-
-echo " hello $P2"
+#referring variable
+echo "$PERSON1:: Hello $PERSON2, How are you?"
+echo "$PERSON2:: Hi $PERSON1, I am fine. How are you?"
+echo "$PERSON1:: I am fine too. how is your work?"
+echo "$PERSON2:: not bad. I am thinking to upgrade to DevOps"
